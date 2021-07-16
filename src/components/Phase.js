9463feb1f3139717e674a5fadd1174ab2adc389f
threehundo <samuelbaik87@gmail.com>
2021-07-16 14:10:12 -0700

    Added notes to Phase component

---
+++ src/components/Phase.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+/**
+ * @param user
+ * 
+ * TODO [] CREATE CALENDAR?
+ * TODO [] CREATE ICONS
+ * TODO [] GET TODAY'S DATE
+ * TODO [] GET THE DATE NEEDED TO CHANGE SLEEPING SCHEDULE
+ * TODO [] CALCULATE WHETHER PHASE DELAY OR ADVANCE IS NEEDED BY TIME ZONE
+ * TODO [] CALCULATE WHEN THE START DATE FOR THE PHASE ADVANCE/DELAY IS NEEDED
+ * TODO [] 
+ * ! Set all your servers’ time zone to UTC.
+ * ! Always use the Server’s time to fill up metadata like created, edited, posted etc.
+ * ! Always store the datetime in UTC time zone.
+ * ! While displaying the datetime to the user, convert it to their local time zone at the application layer.
+ * ! Never use timestamps to store past or future.
+ * ! Try to use datetime format instead of timestamps as it makes visual debugging easy. With timestamp, you have to convert from seconds to date but datetime is like a string. So, if you find yourself accessing the raw values for debugging, then try to use datetime. 
+ */
+
+export const Phase = (user) => {
+	return (
+		<div>
+			
+		</div>
+	)
+}
+
