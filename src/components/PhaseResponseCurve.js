8c1f2fc51909b329a4215b3fc24980bb353cc4e4
threehundo <samuelbaik87@gmail.com>
2021-09-21 21:22:19 -0700

    Pie chart visual created

--- src/components/PhaseResponseCurve.js
+++ src/components/PhaseResponseCurve.js
@@ -1,29 +1,36 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { csv } from "d3";
+import React, { useEffect, useRef, useState } from "react";
+import { arc, csv, pie } from "d3";
 
-const colorUrl = 'https://gist.githubusercontent.com/curran/b236990081a24761f7000567094914e0/raw/cssNamedColors.csv';
+const colorUrl =
+  "https://gist.githubusercontent.com/curran/b236990081a24761f7000567094914e0/raw/cssNamedColors.csv";
+
+const width = 960,
+  height = 500,
+  centerX = width / 2,
+  centerY = height / 2,
+  colorPie = pie().value(1),
+  pieArc = arc().innerRadius(0).outerRadius(width);
 
 export const PhaseResponseCurve = () => {
-	const [colorData, colorDataSet] = useState();
+  const [colorData, colorDataSet] = useState();
 
-	useEffect(() => {
-		csv(colorUrl).then(data => {
-			colorDataSet(data);
-		})
-	},[]);
+  useEffect(() => {
+    csv(colorUrl).then((data) => {
+      colorDataSet(data);
+    });
+  }, []);
 
-	if(!colorData) {
-		return <pre>Loading...</pre>
-	}
+  if (!colorData) {
+    return <pre>Loading...</pre>;
+  }
 
-	return (colorData.map((color, i) => 
-	<div 
-		key={i} 
-		style={{
-			fontSize: "5em", 
-			backgroundColor: color["RGB hex value"], 
-			width: "500px", 
-			height: "10px"}}>
-
-		</div>))
-}
+  return (
+    <svg width={width} height={height}>
+      <g transform={`translate(${centerX},${centerY})`}>
+        {colorPie(colorData).map((color, i) => (
+          <path key={i} fill={color.data["RGB hex value"]} d={pieArc(color)} />
+        ))}
+      </g>
+    </svg>
+  );
+};
