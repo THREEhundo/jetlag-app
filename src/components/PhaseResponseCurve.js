7579d6d16ca8fe43306d7bf66e851022f57b83f3
threehundo <samuelbaik87@gmail.com>
2021-09-21 19:07:34 -0700

    Render Dataset

--- src/components/PhaseResponseCurve.js
+++ src/components/PhaseResponseCurve.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import d3, { csv } from "d3";
-import { message } from './message';
+import { csv } from "d3";
 
 const colorUrl = 'https://gist.githubusercontent.com/curran/b236990081a24761f7000567094914e0/raw/cssNamedColors.csv';
 
@@ -18,11 +17,13 @@ export const PhaseResponseCurve = () => {
 	}
 
 	return (colorData.map((color, i) => 
-	<div key={i} style={{
-		fontSize: "5em", 
-		backgroundColor: color["RGB hex value"], 
-		width: "500px", 
-		height: "10px"}}>
+	<div 
+		key={i} 
+		style={{
+			fontSize: "5em", 
+			backgroundColor: color["RGB hex value"], 
+			width: "500px", 
+			height: "10px"}}>
 
 		</div>))
 }
--- src/components/message.js
+++
@@ -1,9 +0,0 @@
-import { csvFormat } from "d3";
-
-export const message = data => {
-	let msg = '';
-	msg = msg + Math.round(csvFormat(data).length / 1024) + ' kb\n';
-	msg = msg + data.length + ' rows\n';
-	msg = msg + data.columns.length + ' columns\n';
-	return msg;
-};
